3cc53e4460b6df65425b75833eab019ace7e2082
Ravi Sharma <ravisha@chalmers.se>
2024-11-26 20:56:45 +0100

    #7 Create template ui template for Register page

---
+++ Client/src/view/Register.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="register-wrapper">
+      <div class="title">
+        <h1>RoomCount</h1>
+      </div>
+      <BContainer class="d-flex justify-content-center align-items-center vh-100">
+        <BCard class="text-center">
+          <h1 class="mb-3">Register an Account</h1>
+          <p class="mb-4">Join RoomCount today and start managing and tracking your rooms</p>
+          <BForm @submit.prevent="register">
+            <div class="email d-flex justify-content-center">
+              <BFormInput id="email" v-model="input.email" type="email" placeholder="Enter your email"
+                required class="custom-input w-75"></BFormInput>
+            </div>
+            <div class="username d-flex justify-content-center">
+              <BFormInput id="username" v-model="input.username" type="text" placeholder="Enter your username"
+                required class="custom-input w-75"></BFormInput>
+            </div>
+            <div class="password d-flex justify-content-center">
+              <BFormInput id="password" v-model="input.password" type="password" placeholder="Enter your password"
+                required class="custom-input w-75"></BFormInput>
+            </div>
+            <BButton type="submit" variant="warning" class="custom-button w-75">Register</BButton>
+          </BForm>
+  
+          <p class="logIn mt-3">
+            If you already have an account, <router-link to="/LogIn">Sign in here</router-link>
+          </p>
+  
+        </BCard>
+      </BContainer>
+  
+      <!-- Toast Notification -->
+      <div class="toast-container position-fixed top-0 end-0 p-3">
+        <div id="liveToast" class="toast bg-dark" role="alert" style="color: white;" aria-live="assertive"
+          aria-atomic="true" :class="{'show': showToast}">
+          <div class="toast-header bg-dark" style="color: white;">
+            <strong class="me-auto">BeerStep</strong>
+            <small>Just now</small>
+            <BButton type="button" class="btn-close" @click="showToast = false" aria-label="Close"></BButton>
+          </div>
+          <div class="toast-body">
+            {{ toastMessage }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  
