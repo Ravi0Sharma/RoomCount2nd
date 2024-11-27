9e6d07b11c356b999869652c318e12403200f1a0
Ravi Sharma <ravisha@chalmers.se>
2024-11-27 17:20:20 +0100

    #9 Fix route not loading issue

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import Components from 'unplugin-vue-components/vite'
-import { BootstrapVueNextResolver } from 'bootstrap-vue-next'
-
+import Components from 'unplugin-vue-components/vite';
+import { BootstrapVueNextResolver } from 'bootstrap-vue-next';
 
 export default defineConfig({
- plugins: [ vue(),
-   Components({
-     resolvers: [BootstrapVueNextResolver()]  }) ],
- build: {
-   rollupOptions: {
-     external: ['bootstrap/dist/css/bootstrap.css']
-   }
- },
- optimizeDeps: {
-   include: ['bootstrap/dist/css/bootstrap.css']
- }
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [BootstrapVueNextResolver()],
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      external: ['bootstrap-vue-next'], // This line ensures bootstrap-vue-next is not bundled in the production build
+    },
+  },
+  optimizeDeps: {
+    include: ['bootstrap-vue-next'], // Make sure bootstrap-vue-next is pre-bundled for development
+  },
 });
