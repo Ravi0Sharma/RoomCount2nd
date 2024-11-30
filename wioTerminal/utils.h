ac64387e98650644a964849c92783d4e1b505cec
Ravi Sharma <ravisha@chalmers.se>
2024-11-30 03:03:40 +0100

    #10 Declare external variables and functions for ultrasonic sensor and entry counting

---
+++ wioTerminal/utils.h
@@ -0,0 +1,9 @@
+#include "Ultrasonic.h"
+
+
+extern Ultrasonic pinUltraSonic;
+extern long RangeInCentimeters; 
+extern int serial_Begin_Rate;
+extern void entries (); 
+extern int entries_count; 
+
