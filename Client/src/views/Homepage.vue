ea70baba26ac439d015b1096a9698cfbb6c9786b
Ravi Sharma <ravisha@chalmers.se>
2024-12-10 23:03:17 +0100

    #15 Add MQTT client to app and refactor logic for message handling

--- Client/src/views/Homepage.vue
+++ Client/src/views/Homepage.vue
@@ -5,7 +5,8 @@
         <p style="color: whitesmoke; font-size: 34px; text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);">Session</p>
         <BCard class="card">
             <div class="stats">
-              <p>Entries: <h2>{{ Entries }}</h2> </p> 
+              <p>Entries:</p>
+              <h2>{{ entries }}</h2>
             </div>
             <BButton class="btn btn-success w-50">Create Session</BButton>
             <BButton class="btn btn-danger w-50">End Session</BButton>
@@ -24,6 +25,10 @@ export default {
   },
   created() {
     this.updateEntries();
+    this.interval = setInterval(this.updateEntries, 1000); 
+  },
+  beforeDestroy() {
+    clearInterval(this.interval); // Clear the interval when the component is destroyed
   },
   methods: {
     updateEntries() {
--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 
 var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/RoomCount';
 var port = process.env.PORT || 3000;
-
 var app = express();
 
 // Connect to MongoDB
@@ -16,6 +15,42 @@ mongoose.connect(mongoURI).catch(function (err) {
 }).then(function () {
     console.log(`Connected to MongoDB with URI: ${mongoURI}`);
 });
+
+const mqtt = require('mqtt');
+const mqttServer = 'mqtt://broker.hivemq.com'; 
+const mqttPort = 1883; 
+
+// Connect to the MQTT broker
+const client = mqtt.connect(mqttServer, { port: mqttPort });
+
+// When connected
+client.on('connect', () => {
+    console.log('Connected to MQTT Broker');
+    
+    // Subscribe to a topic
+    client.subscribe('RoomCount/1/entry', (err) => {
+        if (!err) {
+            console.log('Subscribed to RoomCount/1/entry');
+        } else {
+            console.log('Failed to subscribe: ' + err);
+        }
+    });
+});
+
+// When a message is received
+client.on('message', (topic, message) => {
+    console.log(`Received message on ${topic}: ${message.toString()}`);
+    // lägg till function för hantera recived
+});
+
+// ksk när max har passat 
+client.publish('RoomCount/1/', ' room!');
+
+// Handle errors
+client.on('error', (err) => {
+    console.error('MQTT connection error: ' + err);
+});
+
 
 // Middleware
 app.use(express.json()); // Parse JSON payloads
