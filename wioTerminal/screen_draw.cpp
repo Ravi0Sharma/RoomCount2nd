6e6a6b838756f7d92b251953f3c1f0286783774a
Ravi Sharma <ravisha@chalmers.se>
2024-12-10 21:44:30 +0100

    #12 Add WIO  screen rendering functions for Wi-Fi and mqtt connection status

--- wioTerminal/screen_draw.cpp
+++ wioTerminal/screen_draw.cpp
@@ -1,4 +1,24 @@
+#include "Screen_draw.h"
+#include "utils.h"
+#include "WiFi.h"
+
 TFT_eSPI tft; // External declaration for TFT_eSPI display object
+
+void Screen_connectingWiFi(){
+tft.setTextSize(3);
+tft.setTextColor(TFT_YELLOW);
+tft.setTextSize(2);
+tft.setCursor((320 - tft.textWidth("Connecting to Wi-Fi..")) / 2, 25);
+tft.print("Connecting to Wi-Fi..");
+}
+
+void Screen_connectingMQTT(){
+tft.fillScreen(TFT_BLACK);
+tft.setTextSize(3);
+tft.setTextSize(2);
+tft.setCursor((320 - tft.textWidth("Connecting to MQTT")) / 2, 25);
+tft.print("Connecting to MQTT");
+}
 
 void Screen_result(int entries_count) {
     tft.fillScreen(TFT_BLACK);
